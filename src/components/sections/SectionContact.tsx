3f4939c5435c97a7d13388caca6dd95f4f10bfb3
vietphung <harryportervn1993@gmail.com>
2025-08-06 21:22:57 +0200

    Update SectionContact component for improved language and user engagement

--- src/components/sections/SectionContact.tsx
+++ src/components/sections/SectionContact.tsx
@@ -9,7 +9,7 @@ export default function SectionContact() {
             </Text>
             <VStack alignItems="flex-start" gap={4}>
                 <Text fontSize="md">
-                    Einen Einblick in meine <b>Projekte und Code-Beispiele</b> erhalten Sie auf{" "}
+                    Wenn du <b>meine Projekte und Code-Beispiele</b> anschauen möchtest, findest du alles auf{" "}
                     <Icon as={FaGithub} color="gray.700" boxSize={5} mb="-2px" />
                     <Link
                         href="https://github.com/quocvietphung"
@@ -27,7 +27,7 @@ export default function SectionContact() {
                 </Text>
 
                 <Text fontSize="md">
-                    Meinen <b>Lebenslauf, Zertifikate und weitere Informationen</b> finden Sie auf{" "}
+                    Meinen <b>Lebenslauf, Zertifikate und mehr über mich</b> findest du auf{" "}
                     <Icon as={FaLinkedin} color="#0077b5" boxSize={5} mb="-2px" />
                     <Link
                         href="https://www.linkedin.com/in/viet-phung-00b740168/"
